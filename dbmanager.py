fe88eca46dc051874494ae485d12595dc6729e97
growlboy <lanin88dima@gmail.com>
2026-07-09 18:45:57 +0300

    ok its not okay(

--- dbmanager.py
+++ dbmanager.py
@@ -3,16 +3,15 @@ import asyncpg
 import aiosql
 import datetime
 
-queries = aiosql.from_path("queries.sql", "asyncpg", mandatory_parameters=False)
-
 class DataBaseManager:
     def __init__(self, pool):
         self.pool = pool
+        self.queries = aiosql.from_path("queries.sql", "asyncpg", mandatory_parameters=False)
 
     async def __get_daily_allow(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                get_daily_allow = await queries.get_daily_allow(conn, tg_id=tg_id)
+                get_daily_allow = await self.queries.get_daily_allow(conn, tg_id=tg_id)
                 return get_daily_allow
         except Exception as error:
             return "Error with getting"
@@ -20,7 +19,7 @@ class DataBaseManager:
     async def __get_today_cal(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                today_cal = await queries.get_today_cal(conn, tg_id=tg_id)
+                today_cal = await self.queries.get_today_cal(conn, tg_id=tg_id)
                 return today_cal
         except Exception as error:
             return "Error with getting"
@@ -28,7 +27,7 @@ class DataBaseManager:
     async def __set_daily_allow(self, tg_id, daily_cal):
         try:
             async with self.pool.acquire() as conn:
-                await queries.set_daily_allow(conn, tg_id=tg_id, daily_cal=daily_cal)
+                await self.queries.set_daily_allow(conn, tg_id=tg_id, daily_cal=daily_cal)
         except Exception as error:
             print(error)
 
@@ -37,42 +36,42 @@ class DataBaseManager:
             await self.__check_today_date(tg_id)
 
             async with self.pool.acquire() as conn:
-                await queries.plus_cal(conn, tg_id=tg_id, plus_cal = plus_cal)
+                await self.queries.plus_cal(conn, tg_id=tg_id, plus_cal = plus_cal)
         except Exception as error:
             print(error)
 
     async def __set_today_cal(self, tg_id, new_cal):
         try:
             async with self.pool.acquire() as conn:
-                await queries.add_user(conn, tg_id=tg_id, today_cal = new_cal)
+                await self.queries.add_user(conn, tg_id=tg_id, today_cal = new_cal)
         except Exception as error:
             print(error)
         
     async def AddUser(self, tg_id, username):
         try:
             async with self.pool.acquire() as conn:
-                await queries.add_user(conn, tg_id=tg_id, username=username)
+                await self.queries.add_user(conn, tg_id=tg_id, username=username)
         except Exception as error:
             print(error)
 
     async def __delete_user(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                await queries.delete_user(conn, tg_id=tg_id)
+                await self.queries.delete_user(conn, tg_id=tg_id)
         except Exception as error:
             print(error)
 
     async def __set_realname(self, tg_id, name):
         try:
             async with self.pool.acquire() as conn:
-                await queries.set_realname(conn, tg_id=tg_id, new_name=name)
+                await self.queries.set_realname(conn, tg_id=tg_id, new_name=name)
         except Exception as error:
             print(error)
      
     async def __get_realname(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                realname = await queries.get_realname(conn, tg_id=tg_id)
+                realname = await self.queries.get_realname(conn, tg_id=tg_id)
                 return realname
         except Exception as error:
             print(error)
@@ -80,20 +79,20 @@ class DataBaseManager:
     async def __check_today_date(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                last_date: datetime.date = await queries.get_user_date(conn, tg_id = tg_id)
-                today_date: datetime.date = await queries.get_current_date(conn)
+                last_date: datetime.date = await self.queries.get_user_date(conn, tg_id = tg_id)
+                today_date: datetime.date = await self.queries.get_current_date(conn)
 
                 if last_date != today_date:
                     self.set_today_cal(tg_id, 0)
 
-                await queries.set_date(conn, tg_id=tg_id)
+                await self.queries.set_date(conn, tg_id=tg_id)
         except Exception as error:
             print(error)
 
     async def __is_register(self, tg_id):
         try:
             async with self.pool.acquire() as conn:
-                return await queries.isregister(conn, tg_id=tg_id)
+                return await self.queries.isregister(conn, tg_id=tg_id)
         except Exception as error:
             print(error)
 
@@ -126,7 +125,7 @@ class DataBaseManager:
         return await self.__is_register(tg_id)
     
     async def IsHaveRealname(self, tg_id):
-        if self.GetRealname(tg_id) == 'None':
+        if await self.GetRealname(tg_id) == 'None':
             return False
         else:
             return True

80abaaa9b446ef3ed074fa3371ea21cfa1284cb6
growlboy <lanin88dima@gmail.com>
2026-07-13 17:01:37 +0300

    continue refactoring

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Router
 
-from start import router as start_router
-from states import router as states_router
-from commands import router as commands_router
-from common import router as common_router
+from .start import router as start_router
+from .states import router as states_router
+from .commands import router as commands_router
+from .common import router as common_router
 
 handlers_router = Router()
 

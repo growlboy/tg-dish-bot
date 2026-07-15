aa3b7d32015ee3758807806c3691ad39914e279a
growlboy <lanin88dima@gmail.com>
2026-07-15 14:27:57 +0300

    test: autodeploy check

--- handlers/common.py
+++ handlers/common.py
@@ -2,6 +2,8 @@ from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 import logging
 from aiogram.enums import ParseMode
+from aiogram.utils.chat_action import ChatActionSender
+
 
 from services.data_process import PlusCallories, IsRegister, GetDayAllow
 from display.error_messages import default_error
@@ -11,33 +13,34 @@ logger = logging.getLogger(__name__)
 router = Router()
 
 @router.message(F.text)
-async def prompt_reading(message: types.Message, db, ai):
+async def prompt_reading(message: types.Message, bot, db, ai):
     try:
         tg_id = message.from_user.id
         prompt = message.text
 
-        if await IsRegister(tg_id, db):
-            answer = await PlusCallories(prompt, tg_id, db, ai)
-            today_allow = await GetDayAllow(tg_id, db)
+        async with ChatActionSender.typing(chat_id=message.chat.id, bot=bot):
+            if await IsRegister(tg_id, db):
+                answer = await PlusCallories(prompt, tg_id, db, ai)
+                today_allow = await GetDayAllow(tg_id, db)
             
-            result_string = "Суточная норма не превышена. Ешь на здоровье!✅"
+                result_string = "Суточная норма не превышена. Ешь на здоровье!✅"
 
-            if int(today_allow) < int(answer[1]):
-                result_string = "Немного превышена суточная норма 🤔"
+                if int(today_allow) < int(answer[1]):
+                    result_string = "Немного превышена суточная норма 🤔"
 
-            text = (
-            f"Это вышло на {answer[0]} каллорий.🥕 \n\n"
-            f"Всего на сегодня: {answer[1]} / {today_allow} каллорий \n\n"
-            f"{result_string}"
-            )
+                text = (
+                f"Это вышло на {answer[0]} каллорий.🥕 \n\n"
+                f"Всего на сегодня: {answer[1]} / {today_allow} каллорий \n\n"
+                f"{result_string}"
+                )
 
-            if answer[0] and answer[1] and today_allow:
-                await message.answer(text=text, parse_mode=ParseMode.HTML)
-            else:
-                await default_error(message)
+                if answer[0] and answer[1] and today_allow:
+                    await message.answer(text=text, parse_mode=ParseMode.HTML)
+                else:
+                    await default_error(message)
         
-        else:
-            await message.answer("Вы еще не зарегистрированы...")
+            else:
+                await message.answer("Вы еще не зарегистрированы...")
 
     except Exception as error:
         print(error)
